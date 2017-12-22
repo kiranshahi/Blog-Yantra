383729602a3aad5ef3c04ed83e611af55374781f
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-22 14:13:05 +0545

    angular 5 project aadded.

--- Blog-Yantra/Startup.cs
+++ Blog-Yantra/Startup.cs
@@ -15,6 +15,7 @@ namespace Blog_Yantra
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
