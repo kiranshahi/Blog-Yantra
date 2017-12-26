629f8a04e60e6b9329108aab0d43dca8df9e5553
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-26 13:48:07 +0545

    db context registered.

--- Blog-Yantra/Startup.cs
+++ Blog-Yantra/Startup.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-
+using Blog_Yantra.Models;
 namespace Blog_Yantra
 {
   public class Startup
@@ -15,17 +11,13 @@ namespace Blog_Yantra
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
       services.AddMvc();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-
       app.UseDefaultFiles();
       app.UseStaticFiles();
       app.UseMvc();
