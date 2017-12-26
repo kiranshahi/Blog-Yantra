d9f3060953fc4ed779e6f3082dc80318e8d2af15
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-26 18:03:35 +0545

    12/26/2017 update.

--- Blog-Yantra/Controllers/TodoController.cs
+++ Blog-Yantra/Controllers/TodoController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Blog_Yantra.Controllers
 {
-  [Route("api/[controlller]")]
+  [Route("api/[Controller]")]
   public class TodoController : Controller
   {
     private readonly TodoContext _context;
@@ -19,6 +19,57 @@ namespace Blog_Yantra.Controllers
         _context.TodoItems.Add(new TodoItem { Name = "Item1" });
         _context.SaveChanges();
       }
+    }
+
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAll()
+    {
+      return _context.TodoItems.ToList();
+    }
+
+    [HttpGet("{id}", Name = "GetTodo")]
+    public IActionResult GetById(long id)
+    {
+      var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+      if (item == null)
+      {
+        return NotFound();
+      }
+      return new ObjectResult(item);
+    }
+
+    // CREATE
+    [HttpPost]
+    public IActionResult Create([FromBody] TodoItem item)
+    {
+      if (item == null)
+      {
+        return BadRequest();
+      }
+      _context.TodoItems.Add(item);
+      _context.SaveChanges();
+      return CreatedAtAction("GetTodo", new { id = item.Id }, item);
+    }
+
+    // Update
+    [HttpPut("{id}")]
+    public IActionResult Update(long id, [FromBody] TodoItem item)
+    {
+      if (item == null || item.Id != id)
+      {
+        return BadRequest();
+      }
+      var todo = _context.TodoItems.FirstOrDefault(t => t.Id == id);
+      if (todo == null)
+      {
+        return NotFound();
+      }
+      todo.IsComplete = item.IsComplete;
+      todo.Name = item.Name;
+
+      _context.TodoItems.Update(todo);
+      _context.SaveChanges();
+      return new NoContentResult();
     }
   }
 }
