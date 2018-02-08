0020a78b6e258cd6da507357f6c2c5fa0fd03446
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-02-08 12:54:51 +0545

    Startup.cs file updated.

--- Startup.cs
+++ Startup.cs
@@ -15,35 +15,11 @@ namespace Blog_Yantra
     {
       services.AddDbContext<BlogContext>(opt => opt.UseInMemoryDatabase("Blog"));
       services.AddMvc();
-
-      // Register the Swagger generator, defining one or more Swagger or more Swagger documents
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new Info 
-        { 
-          Version = "v1",
-          Title = "My Blog API",
-          Description ="A simple blog developed using ASP.NET Core Web API and Angular",
-          TermsOfService = "None",
-          Contact = new Contact { Name = "kiran Shahi", Email = "contact@example.com", Url = "http://kirans.com.np" },
-          License = new License { Name = "GNU Lesser General Public License v3.0", Url = "https://www.gnu.org/licenses/lgpl-3.0.en.html" }
-        });
-      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app)
     {
-
-      // Enale middleware to serve generated Swagger as a JSON endpoint.
-      app.UseSwagger();
-
-      // Enable middleware to server swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-      app.UseSwaggerUI(c => 
-      {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Blog API V1");
-      });
-
       app.UseDefaultFiles();
       app.UseStaticFiles();
       app.UseMvc();
