5cbcf2408f509a70fe2ca80046d69c71d2eea421
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-02-15 17:59:39 +0545

    postcontroller added.

--- Startup.cs
+++ Startup.cs
@@ -20,8 +20,6 @@ namespace Blog_Yantra
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app)
     {
-      app.UseDefaultFiles();
-      app.UseStaticFiles();
       app.UseMvc();
     }
   }
