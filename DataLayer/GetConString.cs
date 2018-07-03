0d7b31fd03dbcaa5e620be87df11878abc923bea
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-07-03 15:50:18 +0545

    connection.json and DataLayer project added.

---
+++ DataLayer/GetConString.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace DataLayer
+{
+    public class GetConString
+    {
+        public static string ApplicationExeDirectory()
+        {
+            var location = Assembly.GetExecutingAssembly().Location;
+            var appRoot = Path.GetDirectoryName(location);
+
+            return appRoot;
+        }
+
+        public static IConfigurationRoot GetAppSettings()
+        {
+            string applicationExeDirectory = ApplicationExeDirectory();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(applicationExeDirectory)
+                .AddJsonFile("connection.json");
+            return builder.Build();
+        }
+    }
+}
