b457245e044882d16a24bf0aeb43724e11538181
Kiran Shahi <kiran.shahi.np@gmail.com>
2018-07-03 16:54:42 +0545

    User model added.

---
+++ DataLayer/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserLogin { get; set; }
+        public string UserPass { get; set; }
+        public string UserNiceName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserUrl { get; set; }
+        public DateTime UserRegistered { get; set; }
+        public string UserActivationKey { get; set; }
+        public int status { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
